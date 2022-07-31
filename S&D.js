de0cb8a9f193308049558cd0c1f30d3ac669f411
jesust <jesustellor@gmail.com>
2022-07-31 15:42:20 -0700

    search and destroy. a function to remove all the items in the array that are in the arguments

---
+++ S&D.js
@@ -0,0 +1,7 @@
+function destroyer(arr, ...args) {
+    return arr.filter(item => !args.includes(item));
+  }  //filter the arr for every item....... thats not ...args include item
+  
+  console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3,));
+
+// this function is to remove all the items in the array that are in the arguments
