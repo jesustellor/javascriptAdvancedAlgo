17451ab796bac5f3de0db411727f584011215b78
jesustellor <jesustellor@gmail.com>
2022-08-09 14:14:22 -0700

    adding comments

--- dnaPairing.js
+++ dnaPairing.js
@@ -1,7 +1,11 @@
 function pairElement(str) {
+  // make const for DNA string
   const dnaRegEx = /[ATCG]+/
+  // check str to match DNA string regex
   if(str.match(dnaRegEx) == str){
+    // function matchedPair to take in a single character and an array.
      const matchedPair = function(char, pairdArray){
+      // checking char to match A, T, C, or G
      switch(char) {
       case "A":
       pairdArray.push(["A", "T"])
@@ -18,11 +22,16 @@ function pairElement(str) {
       default:
        }
      }
+     // create empty array to push paired DNA to
  const paired = []
+  // loop through str and call matchedPair function on each 
+  //character and pass in empty array as second argument
  for(let i = 0; i < str.length; i++){
    matchedPair(str[i], paired)
  }
+ // return paired array with paired DNA
  return paired
+ // if str does not match DNA string regex return error message
  } else {
    return 'DNA Sequence Incorrect'
  }
@@ -31,5 +40,5 @@ function pairElement(str) {
  
  
  
- 
+ // Test Cases
  console.log(pairElement("ACLGA"));
