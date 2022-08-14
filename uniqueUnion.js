deb79cf839270f84894da9f0cb85fec3f46a406c
jesust <jesustellor@gmail.com>
2022-08-13 19:46:55 -0700

    combining array and returning only unique values

---
+++ uniqueUnion.js
@@ -0,0 +1,20 @@
+//function to combine array and return a new array with original values in order of the original array.
+//using the spread operator to pass multiple arugments to the function.
+function uniteUnique(...arr) {
+    // combine array with flat()
+    const combined = arr.flat();
+    // create empty array to store the unique values
+    const newArr = [];
+    // loop through combined array
+     for(let i = 0; i < combined.length; i++){
+    // if empty array indexOf combined array is not there
+      if(newArr.indexOf(combined[i]) == -1){
+        // push combined array value to new array
+        newArr.push(combined[i])
+      }
+    }
+    // return new array
+    return newArr
+  }
+  
+  console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
