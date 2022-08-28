3ba208f7faf92305073f0ea848964d42ad6c6eed
jesust <jesustellor@gmail.com>
2022-08-27 20:51:20 -0700

    adding all the odd numbers in fibonacci sequence up to the number passed in

--- fibonacciAdd.js
+++ fibonacciAdd.js
@@ -1,15 +1,27 @@
 const sumFibs = n => {
+    //declaring variables & place holders
     const fibArray = [0, 1];
     let prev = fibArray.length -2
     let current = prev + 1
-    const sum = 0;
-    for(let i = 0; i < n; i++){
+    let sum = 0;
+    // Creating Fibonacci sequence as big as fibArray gets.
+    for(let i = 0; i < fibArray.length; i++){
+      if(current <= n){
       const nextNum = prev + current;
       fibArray.push(nextNum);
       prev = current;
-      current = nextNum;    
+      current = nextNum; 
     }
-    return fibArray;
+  }
+    // remove last item in array and add all odd numbers
+    if(current > n){
+      fibArray.pop()
+      for(let i = 0; i < fibArray.length; i++){
+      if (fibArray[i] % 2 === 1) {
+      sum = sum + fibArray[i];
+      }
+     } 
+    } 
+  return sum;
   };
-  
-  console.log(sumFibs(10))
+  console.log(sumFibs(4))
